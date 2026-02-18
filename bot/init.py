444fae0e09968124ad519860c9786989d2dfa5d3
dgush <dgushch@gmail.com>
2026-02-18 19:54:14 +0400

    С прокси

--- bot/init.py
+++ bot/init.py
@@ -2,12 +2,20 @@ from telethon import TelegramClient, events
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from zoneinfo import ZoneInfo
+import base64
 
 
 from settings import conf
 
 
-client = TelegramClient(conf.session_name, conf.api_id, conf.api_hash)
+proxy = (
+    "mtproto",
+    "95.217.135.241",
+    443,
+    base64.b64decode("7pJZSUjIp-43RmluNUkNmWNkbnMuZ11vZ2xlLmNvbQ==")
+)
+
+client = TelegramClient(conf.session_name, conf.api_id, conf.api_hash, proxy=proxy)
 
 jobstores = {
     "default": SQLAlchemyJobStore(
