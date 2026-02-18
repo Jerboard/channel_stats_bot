089d71cfbfc68ea6167a84da80cc16c1d82f8f6a
dgush <dgushch@gmail.com>
2026-02-18 19:11:04 +0400

    Первая версия постит каждый час

--- bot/tasks.py
+++ bot/tasks.py
@@ -61,7 +61,7 @@ def schedule_message_check(chat_id: int, message_id: int, delay_minutes: int = 1
         trigger="date",
         args=[chat_id, message_id],
         # run_date=datetime.now() + timedelta(minutes=delay_minutes),
-        run_date=datetime.now() + timedelta(days=1),
+        run_date=datetime.now() + timedelta(hours=1),
         id=f"msg_{chat_id}_{message_id}",
         replace_existing=True,
     )
